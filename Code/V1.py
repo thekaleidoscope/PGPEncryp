84e4e49c5351fa587771cbef68ce3e7c2c04b31e
thekaleidoscope <yadhuksp@gmail.com>
2018-03-12 02:46:51 +0530

    First Working Product

--- Code/V1.py
+++ Code/V1.py
@@ -9,42 +9,48 @@ def getdata(keyfile,PublicKey):
     key = pgpy.PGPKey.new(PubKeyAlgorithm.RSAEncryptOrSign, 4096)
     uid = pgpy.PGPUID.new(name,email=_email)
 
-    pk=key.add_uid(uid,usage={KeyFlags.Sign}, hashes=[HashAlgorithm.SHA256], ciphers=[SymmetricKeyAlgorithm.AES256])
-    kf=open(keyfile,"w")
-    kf.write(str(key))
-    pkf=open(PublicKey,"w")
-    pkf.write(str(uid)+"\n"+str(key.pubkey))
+    pk=key.add_uid(uid,usage={KeyFlags.Sign, KeyFlags.EncryptCommunications, KeyFlags.EncryptStorage}, hashes=[HashAlgorithm.SHA256], ciphers=[SymmetricKeyAlgorithm.AES256])
+    kf=open(keyfile,"wb")
+    kf.write(bytes(key))
+    pkf=open(PublicKey,"wb")
+    pkf.write(bytes(key.pubkey))
     #print(key.pubkey)
+def get_key(name):
+      key = pgpy.PGPKey.from_file("{}.asc".format(name))[0]
+      return key
+
 i=input("New User?(Y/N)")
 if i=="Y" or i=="y":
-    getdata("PriKey.txt","Pubkey.txt")
-def encrypt(data, key):
-    k = Encryption.get_key(key)
+    getdata("PriKey.asc","Pubkey.asc")
+def encrypt(data, key="PriKey"):
+    k = get_key(key)
     m = k.pubkey.encrypt(pgpy.PGPMessage.new(data), cipher=SymmetricKeyAlgorithm.AES256)
-    return str(m)
+    return bytes(m)
 
-def decrypt(data, key):
-    k = Encryption.get_key(key)
+def decrypt(data, key="PriKey"):
+    k = get_key(key)
     m = k.decrypt(pgpy.PGPMessage.from_blob(data))
     return bytes(m._message.contents) if isinstance(m._message.contents, bytearray) else m._message.contents
+
 while(True):
     e=input("Encrypt(E)/Decrypt(D)")
-    
-    print("Key:",)
-    lines = []
-    while True:
-        line = input()
-        if line:
-            lines.append(line)
-        else:
-            break
-    k = '\n'.join(lines)
 
-    d=input("data:")
     if e=="E":
-        encrypt(data,key)
+        print("data:")
+        lin = []
+        while True:
+            line = input()
+            if line:
+                lin.append(line)
+            else:
+                break
+        d = '\n'.join(lin)
+        ed=encrypt(d)
+        open("EncData.asc","wb").write(ed)
+
     elif e=="D":
-        decrypt(data,key)
+        d=open("EncData.asc","rb").read()
+        print(str(decrypt(d)))
 
     else:
         print("ReRead the options")
